2d8f51af01b6b985c7a2a8381b43ebef2b9e55cd
noxlux <prattyushgoswami.coffee@gmail.com>
2026-04-11 17:12:39 +0530

    misc

--- Source-code/command handler.py
+++ Source-code/command handler.py
@@ -12,11 +12,13 @@ intents.message_content = True                                # Allows the bot t
 # Bot is initialized to the command with prefix [!]
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+'''
 # Bot is initialized to command !sync (Required during development phase)
 @bot.command()
 async def sync(ctx: commands.Context):
     x = await ctx.bot.tree.sync()
     await ctx.send(f'Synced {len(x)} commands')
+'''
 
 #Loads all the created commands ending with extension .py
 async def load():
